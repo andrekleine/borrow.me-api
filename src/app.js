9582dc3b0dbc01a74137d8c2fdf42b43ae4d233e
André Kleine <andrekleine@gmail.com>
2021-11-26 14:43:14 -0300

    Add app.js middlewares

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-// Calling environment variables
-dotenv.config();
 
+import errorHandlingMiddleware from './middlewares/errorHandling';
+import requestTrackMiddleware from './middlewares/requestTracking';
+import resourceNotFoundMiddleware from './middlewares/resourceNotFound';
+
+dotenv.config();
 const app = express();
 
 // Middlewares
-app.use((req, res, next) => {
-  console.log(`Receiving ${req.method} request to route ${req.path}`);
-  next();
+app.use(requestTrackMiddleware);
+
+
+app.get('/', (req, res, next) => {
+  try {
+    res.json({
+      message: 'Hello borrow.me API!',
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello borrow.me API!',
-  })
-});
-
-// Error handling middleware
-// Route not found middleware
-
+app.use(errorHandlingMiddleware);
+app.use(resourceNotFoundMiddleware);
 
 app.listen(process.env.PORT, () =>
   console.log(`App connected to PORT ${process.env.PORT}`)
---
+++ src/middlewares/resourceNotFound.js
@@ -0,0 +1,7 @@
+const resourceNotFoundMiddleware = (req, res) => {
+  res
+    .status(404)
+    .json({ message: `Resource ${req.method} to '${req.path}' not found.` });
+};
+
+export default resourceNotFoundMiddleware;
