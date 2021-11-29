ac8fc7cb8d01490a12753ba0dcaa1fb629f0efe7
André Kleine <andrekleine@gmail.com>
2021-11-29 18:52:47 -0300

    minor adjustments

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import authController from './controllers/authController';
 import booksController from './controllers/booksController';
@@ -6,7 +6,29 @@ import reviewsController from './controllers/reviewsController';
 
 const router = Router();
 
+// Public routes
 router.use('/auth', authController);
+
+// Protected routes middleware
+router.use((req, res, next) => {
+  try {
+    if (!token) {
+      throw new Error();
+    }
+
+    if (!tokenIsValid) {
+      throw new Error();
+    }
+
+    // Insert user info inside request
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Private routes
 router.use('/books', booksController);
 router.use('/reviews', reviewsController);
 
